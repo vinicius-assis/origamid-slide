484256d61642007f8ff3d10182e66974bb825cc4
Vinicius Assis <believe.182@hotmail.com>
2019-09-18 13:31:12 -0300

    Finish slide

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-import Slide from './slide.js'
+import SlideNav from './slide.js'
 
-const slide = new Slide('.slide', '.wrapper')
+const slide = new SlideNav('.slide', '.wrapper')
 slide.init()
+slide.addArrow('.prev', '.next')
+slide.addControl('.custom-controls')
